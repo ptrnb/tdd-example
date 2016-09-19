5ff7f0e6eb982f36c594c684bf9497811ab52461
Peter Brown <peternbrown@mac.com>
2016-09-19 19:28:56 +1000

    First unit test and url mapping, dummy view

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,37 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicity_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app
+        # She goes to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+        # She type "Buy peacock feathers" into a text box
+        # When she hits enter, the page updates and now the page
+        # lists "1: Buy peacock feathers" as an item in the to-do list
+        # There is still a text box inviting her to add another item, she enters
+        # "use peacock feathers to make a fly"
+        # The page updates again, shows both items on her list
+        # Edith wonders whether the site will remember her list, then sees
+        # that the site has generated a unique URL for her -- there is 
+        # some explanatory text to that effect
+        # She visits that URL - her to-do list is still there
+        # Satisfied, she goes back to sleep
+        #
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
