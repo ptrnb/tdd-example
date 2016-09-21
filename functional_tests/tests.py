07f8af1b6abdb716b257d4f2ebd24b3908acd066
Peter Brown <peternbrown@mac.com>
2016-09-21 21:01:57 +1000

    Created new lists view

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -8,8 +8,6 @@ class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        # self.browser = Safari()
-        # self.browser.implicity_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -53,20 +51,20 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, shows both items on her list
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # Now a new user, Francis, comes along
         #
         # We use a new browser session to make sure no information
         # belonging to Edith is coming through from cookies
         self.browser.quit()
-        self.browser = webdriver.Firefox
+        self.browser = webdriver.Firefox()
 
         # Francis visits the home page. There is no sign of 
         # Edith's list
         self.browser.get(self.live_server_url)
-        page_text = self.browser_find_element_by_tag_name('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
@@ -77,6 +75,11 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
