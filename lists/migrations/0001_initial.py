0303cf5e30f56313926be0633bef00145165fdf3
Peter Brown <peternbrown@mac.com>
2016-09-23 20:45:41 +1000

    Fixes for Chapter 6

--- lists/migrations/0001_initial.py
+++ lists/migrations/0001_initial.py
@@ -13,7 +13,19 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Item',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('text', models.TextField(default='')),
             ],
+        ),
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='list',
+            field=models.ForeignKey(to='lists.List', default=None),
         ),
     ]
